68b7d11511378d6dd9130a61aa9ea46fd28d4d4e
Jonas Hagstedt <hagstedt@gmail.com>
2015-07-15 16:04:19 +0200

    Fixed bug with test runner (no results returned!)

--- nodetest/test_case.py
+++ nodetest/test_case.py
@@ -4,4 +4,4 @@ from .node_runner import process_script
 
 class NodeTestCase(LiveServerTestCase):
     def run_test_script(self, script_file, plaintext=False, enable_console=False):
-        process_script(script_file, plaintext, enable_console)
+        return process_script(script_file, plaintext, enable_console)
